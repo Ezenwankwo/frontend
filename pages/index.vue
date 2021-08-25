67075956056a67436674bc55f5261cbc43f1252c
augustus ezenwankwo <onyeka.ezenwankwo@gmail.com>
2021-08-25 06:24:59 +0000

    working on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <Tutorial />
+  <b-container>
+    <b-row>
+      <b-col cols="6">
+        <p>
+          Townsmeet is where you connect and engage with the people, businesses
+          and public authorities in your town. Have conversations about what
+          affects you where you live, work, school and trade.
+        </p>
+      </b-col>
+      <b-col cols="6"></b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
