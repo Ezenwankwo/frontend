9100d7540a2a3c7f8e14c7d6e7e9d7b3c6d7bc10
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2021-08-25 18:15:46 +0100

    first heroku deploy

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,19 @@
 <template>
   <b-container>
     <b-row>
-      <b-col cols="6">
+      <b-col cols="6" class="mt-5">
         <p>
           Townsmeet is where you connect and engage with the people, businesses
           and public authorities in your town. Have conversations about what
           affects you where you live, work, school and trade.
         </p>
       </b-col>
-      <b-col cols="6"></b-col>
+      <b-col cols="6" />
+      </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
