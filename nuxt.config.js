1fe6c19ed8a4b4ddb5099dd7fd576a5fa51a0d4e
Wixi105 <ericsampson105@gmail.com>
2022-02-14 09:13:45 +0000

    added new modules (dayjs, google-analytics,vue-toastification)
    Signed-off-by: Wixi105 <ericsampson105@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,13 +37,26 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+
+    '@nuxtjs/google-analytics',
+
+    '@nuxtjs/dayjs',
+
+    'cooke-universal-nuxt',
+
+    'vue-toastification/nuxt'
+
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/'
+  },
+
+  toast: {
+    // Vue toastification plugin options
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
