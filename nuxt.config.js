df92598f6f26aa61842a992da466d74b7df36f0f
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2022-09-07 07:40:43 -0400

    fix: move filepond css to configuration file

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    'filepond/dist/filepond.min.css',
+    'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
